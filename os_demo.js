092a39672f3cac1b62d103a385cb7478dcd569a6
mohdsajeed <mohdsajeed871@gmail.com>
2023-12-10 18:40:44 +0530

    first commit

---
+++ os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+//Platform
+console.log(os.platform());
+
+//CPU Arch
+console.log(os.arch());
+
+//CPU Core Info
+console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+//Total Memory
+console.log(os.totalmem())
+
+//Home Dir
+console.log(os.homedir());
+
+//uptime 
+console.log(os.uptime());
---
+++ person.js
@@ -0,0 +1,6 @@
+const Person=require('./person');
+import Person from './person';
+
+const person1 = new Person('harry potter',30);
+
+person1.greeting();
